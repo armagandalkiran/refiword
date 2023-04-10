49210e9d0eb7caa6c3d0f6d1b7e9cd0bbc9b0240
Armağan Dalkıran <armagandalkran20@gmail.com>
2023-04-10 22:56:56 +0300

    chore: REFI-14 socket server url change

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  process.env.NODE_ENV === "production" ? undefined : "https://refiword-server.onrender.com";
-
+const URL = "https://refiword-server.onrender.com";
 
 export const socket = io(URL as any, {
   autoConnect: false,
